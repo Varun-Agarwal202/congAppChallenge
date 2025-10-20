561714a6300afa36a7b42eb3aa36dfc00f15654c
Varun Agarwal <varunagarwal@mac.lan>
2025-10-19 18:24:28 -0600

    add navbar

--- app/urls.py
+++ app/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from app.views import home
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('app.urls'), name='home'),
+    path('', home, name='home'),
 ]
